ebc86b145bc61b296235d68930c8d7ceb6e21202
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-17 13:37:45 +0700

    membuat Layout dan fungsi button close dan new

---
+++ tokoMainan/tokoMainan/Form1.Designer.vb
@@ -0,0 +1,329 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.kode = New System.Windows.Forms.TextBox()
+        Me.harga = New System.Windows.Forms.TextBox()
+        Me.subTotal = New System.Windows.Forms.TextBox()
+        Me.jumlah = New System.Windows.Forms.TextBox()
+        Me.total = New System.Windows.Forms.TextBox()
+        Me.pajak = New System.Windows.Forms.TextBox()
+        Me.nama = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.dataTabel = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dataTabel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label1.Location = New System.Drawing.Point(12, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(452, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "TOKO MAINAN AMAN SEJAHTERA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Garamond", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label2.Location = New System.Drawing.Point(14, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(278, 14)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jl. Sakeloa No. 56 Bandung No. Telp (022) 73281 292"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.nama)
+        Me.GroupBox1.Controls.Add(Me.total)
+        Me.GroupBox1.Controls.Add(Me.pajak)
+        Me.GroupBox1.Controls.Add(Me.subTotal)
+        Me.GroupBox1.Controls.Add(Me.jumlah)
+        Me.GroupBox1.Controls.Add(Me.harga)
+        Me.GroupBox1.Controls.Add(Me.kode)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 97)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(300, 246)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "| Input Data |"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(16, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 16)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "kode Transaksi"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(16, 60)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 16)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Nama Mainan"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(16, 118)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 16)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Jumlah"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(16, 90)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 16)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "harga"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(16, 202)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 16)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Total"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(16, 174)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(42, 16)
+        Me.Label8.TabIndex = 5
+        Me.Label8.Text = "pajak"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(16, 146)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(64, 16)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "sub Total"
+        '
+        'kode
+        '
+        Me.kode.Location = New System.Drawing.Point(125, 29)
+        Me.kode.Name = "kode"
+        Me.kode.Size = New System.Drawing.Size(73, 22)
+        Me.kode.TabIndex = 7
+        '
+        'harga
+        '
+        Me.harga.Location = New System.Drawing.Point(125, 87)
+        Me.harga.Name = "harga"
+        Me.harga.Size = New System.Drawing.Size(144, 22)
+        Me.harga.TabIndex = 8
+        '
+        'subTotal
+        '
+        Me.subTotal.Location = New System.Drawing.Point(125, 143)
+        Me.subTotal.Name = "subTotal"
+        Me.subTotal.Size = New System.Drawing.Size(144, 22)
+        Me.subTotal.TabIndex = 10
+        '
+        'jumlah
+        '
+        Me.jumlah.Location = New System.Drawing.Point(125, 115)
+        Me.jumlah.Name = "jumlah"
+        Me.jumlah.Size = New System.Drawing.Size(73, 22)
+        Me.jumlah.TabIndex = 9
+        '
+        'total
+        '
+        Me.total.Location = New System.Drawing.Point(125, 199)
+        Me.total.Name = "total"
+        Me.total.Size = New System.Drawing.Size(144, 22)
+        Me.total.TabIndex = 12
+        '
+        'pajak
+        '
+        Me.pajak.Location = New System.Drawing.Point(125, 171)
+        Me.pajak.Name = "pajak"
+        Me.pajak.Size = New System.Drawing.Size(144, 22)
+        Me.pajak.TabIndex = 11
+        '
+        'nama
+        '
+        Me.nama.FormattingEnabled = True
+        Me.nama.Location = New System.Drawing.Point(125, 57)
+        Me.nama.Name = "nama"
+        Me.nama.Size = New System.Drawing.Size(144, 24)
+        Me.nama.TabIndex = 13
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnClose)
+        Me.GroupBox2.Controls.Add(Me.btnDelete)
+        Me.GroupBox2.Controls.Add(Me.btnSave)
+        Me.GroupBox2.Controls.Add(Me.btnNew)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GroupBox2.Location = New System.Drawing.Point(332, 97)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(132, 246)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "| Proses |"
+        '
+        'btnNew
+        '
+        Me.btnNew.ForeColor = System.Drawing.Color.Black
+        Me.btnNew.Location = New System.Drawing.Point(16, 47)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(97, 34)
+        Me.btnNew.TabIndex = 0
+        Me.btnNew.Text = "New"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.ForeColor = System.Drawing.Color.Black
+        Me.btnSave.Location = New System.Drawing.Point(16, 93)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(97, 34)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.ForeColor = System.Drawing.Color.Black
+        Me.btnClose.Location = New System.Drawing.Point(16, 182)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(97, 34)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.ForeColor = System.Drawing.Color.Black
+        Me.btnDelete.Location = New System.Drawing.Point(16, 136)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(97, 34)
+        Me.btnDelete.TabIndex = 2
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'dataTabel
+        '
+        Me.dataTabel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataTabel.Location = New System.Drawing.Point(13, 350)
+        Me.dataTabel.Name = "dataTabel"
+        Me.dataTabel.Size = New System.Drawing.Size(451, 132)
+        Me.dataTabel.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(489, 502)
+        Me.Controls.Add(Me.dataTabel)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Toko Mainan Aman Sejahtra"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.dataTabel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents nama As ComboBox
+    Friend WithEvents total As TextBox
+    Friend WithEvents pajak As TextBox
+    Friend WithEvents subTotal As TextBox
+    Friend WithEvents jumlah As TextBox
+    Friend WithEvents harga As TextBox
+    Friend WithEvents kode As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnNew As Button
+    Friend WithEvents dataTabel As DataGridView
+End Class
---
+++ tokoMainan/tokoMainan/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+    Private Sub BtnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        kode.Clear()
+        nama.Text = " "
+        harga.Clear()
+        jumlah.Clear()
+        subTotal.Clear()
+        pajak.Clear()
+        total.Clear()
+        MessageBox.Show("Data Baru", "Data", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+End Class
