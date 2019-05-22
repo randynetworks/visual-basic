643edbea4856ef4ac7fbbf3ad09a797fc0fba5bc
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-22 08:36:50 +0700

    buat fungsi baru dan close

--- pertemuan10/pertemuan10/pertemuan10/Form1.Designer.vb
+++ pertemuan10/pertemuan10/pertemuan10/Form1.Designer.vb
@@ -162,6 +162,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
--- pertemuan10/pertemuan10/pertemuan10/Form1.vb
+++ pertemuan10/pertemuan10/pertemuan10/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
 
+    Private Sub BtnBaru_Click(sender As Object, e As EventArgs) Handles btnBaru.Click
+        rm.Clear()
+        nama.Clear()
+        alamat.Clear()
+        MessageBox.Show("Data Baru", "Data", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
